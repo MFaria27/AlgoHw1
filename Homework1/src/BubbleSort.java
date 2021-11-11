a6bc362ad2b75c112dc8366cfc84b368aa41ba6f
MFaria27 <mattheusfaria27@gmail.com>
2021-11-11 00:54:41 -0500

    Finished commenting code

--- Homework1/src/BubbleSort.java
+++ Homework1/src/BubbleSort.java
@@ -10,8 +10,11 @@ public class BubbleSort {
 	public void sort(Comparable[] a) {
 		for (int i = 0; i < a.length - 1; i++) {
 			for (int j = 0; j < a.length - i - 1; j++) {
-				bSteps++;
-				if(less(a[j+1], a[j])) {
+				bSteps++; //A step will be recorded every time a pair of values is compared.
+				//Bubble sort compares a pair of values every iteration. If the value in front of the other is larger, the two values are swapped.
+				//This continues to happen until the largest value is "bubbled" up to the end of the array.
+				//After traversing the entire array, all values will be properly bubbled-up and the array will be sorted
+				if(less(a[j+1], a[j])) { 
 					exch(a, j, j+1);
 				}
 			}
@@ -19,32 +22,32 @@ public class BubbleSort {
 	}
 	
 	//The Less(v, w) function returns true if v is less than w using eclipse's compareTo() function.
-		private static boolean less(Comparable v, Comparable w) {
-			return v.compareTo(w) < 0;
-		}
+	private static boolean less(Comparable v, Comparable w) {
+		return v.compareTo(w) < 0;
+	}
 
-		//The exch, or exchange function, takes an array and the indices that the array will swap.
-		//It does this by setting the first value to a placeholder variable, setting the first value as the second value, then setting the second value to the placeholder variable.
-		private static void exch(Comparable[] a, int i, int j) {
-			Comparable t = a[i];
-			a[i] = a[j];
-			a[j] = t;
-		}
+	//The exch, or exchange function, takes an array and the indices that the array will swap.
+	//It does this by setting the first value to a placeholder variable, setting the first value as the second value, then setting the second value to the placeholder variable.
+	private static void exch(Comparable[] a, int i, int j) {
+		Comparable t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
 
-		//The show function takes an array and prints all the values ({"C", "A", "T"} will display "CAT").
-		public void show(Comparable[] a) {
-			for (Comparable element : a)
-				StdOut.print(element + " ");
-			StdOut.println();
-		}
+	//The show function takes an array and prints all the values ({"C", "A", "T"} will display "CAT").
+	public void show(Comparable[] a) {
+		for (Comparable element : a)
+			StdOut.print(element + " ");
+		StdOut.println();
+	}
 
-		//The isSorted function returns a boolean value that represents whether or not an array has been sorted.
-		//It checks this by using the less() function, and making sure that every value is less than the next. 
-		//If it is not, it will return false.
-		private static boolean isSorted(Comparable[] a) {
-			for (int i = 1; i < a.length; i++) {
-				if (less(a[i], a[i-1])) return false;
-			}
-			return true;
+	//The isSorted function returns a boolean value that represents whether or not an array has been sorted.
+	//It checks this by using the less() function, and making sure that every value is less than the next. 
+	//If it is not, it will return false.
+	private static boolean isSorted(Comparable[] a) {
+		for (int i = 1; i < a.length; i++) {
+			if (less(a[i], a[i-1])) return false;
 		}
+		return true;
+	}
 }
--- Homework1/src/MergeSort.java
+++ Homework1/src/MergeSort.java
@@ -9,17 +9,20 @@ public class MergeSort {
 	}
 	
 	public void sort(Comparable[] a) {
+		//Create an auxiliary array to store a copy of array
 		aux = new Comparable[a.length];
 		mergeSort(a, 0, a.length-1);
 	}
 	
 	private void mergeSort(Comparable[] a, int low, int high) {
-		mSteps++;
-		//System.out.println(mSteps);
-		if(high <= low) return;
-		int mid = low + (high - low)/2;
-		mergeSort(a, low, mid);
-		mergeSort(a, mid+1, high);
+		mSteps++; //Every time Merge Sort partitions and sorts the array will count as a step. (We can also count the merging, but that's not necessarily going through the array).
+		if(high <= low) return; //If the low point is greater than the high point, either something failed, or nothing was found.
+		int mid = low + (high - low)/2; //Merge sort selects the mid point of the current array between the low and high points
+		//The sorter will then split the array in half, and will continue to do so with both halves of the array.
+		//This will recursively continue until all array values are split.
+		mergeSort(a, low, mid); 
+		mergeSort(a, mid+1, high); 
+		//Then Merge Sort will compare each split and merge them together in the right order, and will do this recursively until the array is sorted.
 		merge(a, low, mid, high);
 	}
 
@@ -37,32 +40,32 @@ public class MergeSort {
 	}
 	
 	//The Less(v, w) function returns true if v is less than w using eclipse's compareTo() function.
-		private static boolean less(Comparable v, Comparable w) {
-			return v.compareTo(w) < 0;
+	private static boolean less(Comparable v, Comparable w) {
+		return v.compareTo(w) < 0;
+	}
+	
+	//The exch, or exchange function, takes an array and the indices that the array will swap.
+	//It does this by setting the first value to a placeholder variable, setting the first value as the second value, then setting the second value to the placeholder variable.
+	private static void exch(Comparable[] a, int i, int j) {
+		Comparable t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+	
+	//The show function takes an array and prints all the values ({"C", "A", "T"} will display "CAT").
+	public void show(Comparable[] a) {
+		for (Comparable element : a)
+			StdOut.print(element + " ");
+		StdOut.println();
+	}
+	
+	//The isSorted function returns a boolean value that represents whether or not an array has been sorted.
+	//It checks this by using the less() function, and making sure that every value is less than the next. 
+	//If it is not, it will return false.
+	private static boolean isSorted(Comparable[] a) {
+		for (int i = 1; i < a.length; i++) {
+			if (less(a[i], a[i-1])) return false;
 		}
-
-		//The exch, or exchange function, takes an array and the indices that the array will swap.
-		//It does this by setting the first value to a placeholder variable, setting the first value as the second value, then setting the second value to the placeholder variable.
-		private static void exch(Comparable[] a, int i, int j) {
-			Comparable t = a[i];
-			a[i] = a[j];
-			a[j] = t;
-		}
-
-		//The show function takes an array and prints all the values ({"C", "A", "T"} will display "CAT").
-		public void show(Comparable[] a) {
-			for (Comparable element : a)
-				StdOut.print(element + " ");
-			StdOut.println();
-		}
-
-		//The isSorted function returns a boolean value that represents whether or not an array has been sorted.
-		//It checks this by using the less() function, and making sure that every value is less than the next. 
-		//If it is not, it will return false.
-		private static boolean isSorted(Comparable[] a) {
-			for (int i = 1; i < a.length; i++) {
-				if (less(a[i], a[i-1])) return false;
-			}
-			return true;
-		}
+		return true;
+	}
 }
